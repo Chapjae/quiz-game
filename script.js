00d76d83adee6dd79484b98527d0a6c3d8480821
Jay Liu <jayliu@Jays-MacBook-Air.local>
2023-07-01 11:45:34 -0700

    successfully populated one sample question and answer demo onto the html

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var score = 0
 var timeLeft = 200;
 var questions = ["How do I test this?"]
-var answerChoices = [["By Answering this", "Or this?", "Perhaps This?", "I don't know?"], [""]] 
+var answerChoices = [
+    ["By Answering this", "Or this?", "Perhaps This?", "I don't know?"], 
+    [""]
+] 
 var correctAnswer = ["By Answering this"]
 var currentQuestion = 0
-
-var questionEl = document.querySelector("question");
-var answers = document.getElementsByClassName("answer");
 
 var playerScore = document.getElementById("score");
 var startGame = document.getElementById("start-game");
@@ -14,10 +14,19 @@ var startGame = document.getElementById("start-game");
 startGame.addEventListener("click", start)
 
 function start() {
-    questionEl = questions[0]
-    playerScore.textContent = score;
-debugger
-    }
+    var question = document.getElementById("question");
+    var answers = document.getElementById("answer-choices")
+    
+    question.textContent = questions[0];
+    answers = answerChoices[0];
+    
+    answers.forEach(choice => {
+        var answers = document.createElement("li");
+        answers.textContent = choice;
+        question.appendChild(answers)
+    });
+    
+}
 
 function nextQuestion(event) {
 }
