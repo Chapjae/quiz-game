63c1fdb94ba0ac837c78dc450add0b14ecf057eb
Jay Liu <jayliu89@gmail.com>
2023-07-01 20:34:32 -0700

    restructured start function. function contains the score and time left display. Will include if statement for score functionality

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 var score = 0
 var timeLeft = 200;
-var questions = ["How do I test this?"]
+var questions = ["How do I test this?", "More Testing", "Test 3", "Test 4", "Test 5"]
 var answerChoices = [
     ["By Answering this", "Or this?", "Perhaps This?", "I don't know?"], 
-    [""]
+    ["Test More Answers", "Answer More Tests", "What About This?", "Hey hey hey"],
+    ["Test 3 Answers", "answer 2 for 3", "answer 3 for 3", "answer 4 for 3"],
+    ["Test 4 Answers", "answer 2 for 4", "answer 3 for 4", "answer 4 for 4"],
+    ["Test 5 Answers", "answer 2 for 5", "answer, 3 for 5", "answer 4 for 5"]
 ] 
-var correctAnswer = ["By Answering this"]
-var currentQuestion = 0
 
 var playerScore = document.getElementById("score");
 var startGame = document.getElementById("start-game");
-var choices = document.getElementById("answers-choices")
-
-
-
+var choices = document.getElementById("answers-choices");
+var question = document.getElementById("question");
+var timer = document.getElementById("timer")
 
 startGame.addEventListener("click", start)
 
 function generateQuestions() {
+    var randomIdx = Math.floor(Math.random() * questions.length)
+    var randomQuestion = questions[randomIdx] 
+
+    question.textContent = randomQuestion
+
     while(choices.firstChild) {
         choices.removeChild(choices.firstChild)
         }
 
-    answerChoices[0].forEach(answer => {
+    answerChoices[randomIdx].forEach(answer => {
         var options = document.createElement("li");
        // var clickOptions = document.querySelectorAll('li')
            options.textContent = answer;
@@ -33,17 +38,13 @@ function generateQuestions() {
 
 
 function start() {
-    var question = document.getElementById("question");
-    
-    question.textContent = questions[0];
-    answers = answerChoices[0];
-
+    playerScore.textContent = `Your Score: ${score}`;
+    timer.textContent = `Time Left!: ${timeLeft}`;
     generateQuestions()
 }
 
 function nextQuestion(e) {
    generateQuestions()
-
 }
 
 function endGame() {
